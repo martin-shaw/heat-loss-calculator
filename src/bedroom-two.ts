4bce5a6c2668dc993c5ff25ca209c9e88bbb30d2
mshaw <martin.shaw@starcompliance.com>
2022-12-30 16:37:22 +0000

    Add bedroom two

---
+++ src/bedroom-two.ts
@@ -0,0 +1,49 @@
+import {
+    doubleBrick,
+    doubleGlazing,
+    internalCeiling,
+    internalFloor,
+    plaster,
+    RdSAPDoubleBrick,
+    RdSAPPartyWall,
+    singleBrick,
+} from "./fabric";
+import { Room } from "./room";
+
+export const room: Room = {
+    name: "Bedroom 2",
+    temperature: 18,
+    surfaces: [
+        {
+            name: "external wall",
+            width: 3.1,
+            height: 2.8,
+            composition: RdSAPDoubleBrick,
+            boundaryTemperature: -3,
+            elements: [{ name: "window 1", width: 1, height: 1.65, composition: doubleGlazing }],
+        },
+        {
+            name: "party wall",
+            width: 3.9,
+            height: 2.8,
+            composition: RdSAPPartyWall,
+            boundaryTemperature: 18,
+        },
+        {
+            name: "internal wall",
+            width: 3.9,
+            height: 2.8,
+            composition: [plaster, singleBrick, plaster],
+            boundaryTemperature: 18,
+        },
+        {
+            name: "internal wall",
+            width: 3.1,
+            height: 2.8,
+            composition: [plaster, singleBrick, plaster],
+            boundaryTemperature: 18,
+        },
+        { name: "internal floor", width: 3.9, height: 3.1, composition: internalFloor, boundaryTemperature: 18 },
+        { name: "internal ceiling", width: 3.9, height: 3.1, composition: internalCeiling, boundaryTemperature: 18 },
+    ],
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { room } from "./bedroom-one";
+import { room as bedroom1 } from "./bedroom-one";
+import { room as bedroom2 } from "./bedroom-two";
 import { calculateRoom } from "./calculator";
 
-console.log(calculateRoom(room));
+console.log(calculateRoom(bedroom1));
+console.log(calculateRoom(bedroom2));
