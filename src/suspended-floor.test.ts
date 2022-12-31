ea3d5f74795ca39cda81eea8035935315216f53d
mshaw <martin.shaw@starcompliance.com>
2022-12-31 13:21:04 +0000

    Update comment to refer to docs

--- src/suspended-floor.test.ts
+++ src/suspended-floor.test.ts
@@ -1,20 +1,9 @@
 import { calculate } from "./suspended-floor";
 
-/**
- * https://www.sciencedirect.com/science/article/pii/S0378778817311350
- * https://eprints.whiterose.ac.uk/116433/14/Suspended%20timber%20ground%20floors%20measured%20heat%20loss%20compared%20with%20models.pdf
- * The case study house was a pre-1919 solid wall terraced house located in a conservation area in London and the case study characteristics,
- * alongside instrumentation of the uninsulated floor and research methods, are described in more detail in [38].
- * The 12.15 m2 living room floor had a floor void depth of 250 mm below the 100 mm joists, with a 150 mm concrete oversite surface on the soil.
- */
-
-// RdSAP value
+// See src/suspended-floor.test.md for model assumptions
 const wallThickness = 0.22;
-// Area is in the article. See comment above
 const area = 27.7;
-// Exposed perimiter has been estimated from the floorplan diagrams.
 const exposedPerimiter = 9.1;
-// Height above ground is in the article (0.25 void below 0.1 joists)
 const heightAboveGround = 0.25;
 const rf = 0.13;
 const ventilation = 0.0024;
