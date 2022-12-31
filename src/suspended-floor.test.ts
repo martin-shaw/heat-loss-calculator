c3e1269d5f82d98aa69b53812d909d3abccca130
mshaw <martin.shaw@starcompliance.com>
2022-12-31 12:46:15 +0000

    Update model data based the more detailed reference https://eprints.whiterose.ac.uk/116433/14/Suspended%20timber%20ground%20floors%20measured%20heat%20loss%20compared%20with%20models.pdf

--- src/suspended-floor.test.ts
+++ src/suspended-floor.test.ts
@@ -9,27 +9,64 @@ import { calculate } from "./suspended-floor";
 
 // RdSAP value
 const wallThickness = 0.22;
-// Area is in the article. See comment abvoe
-const area = 12.15;
+// Area is in the article. See comment above
+const area = 27.7;
 // Exposed perimiter has been estimated from the floorplan diagrams.
-const exposedPerimiter = 3;
+const exposedPerimiter = 9.1;
 // Height above ground is in the article (0.25 void below 0.1 joists)
-const heightAboveGround = 0.35;
+const heightAboveGround = 0.25;
+const rf = 0.13;
+const ventilation = 0.0024;
+const wallUValue = 1.7;
 
 describe("Science Direct case study RdSAP calibration", () => {
     test("Uninsulated floor", () => {
-        const result = calculate(wallThickness, area, exposedPerimiter, heightAboveGround);
+        const result = calculate({
+            wallThickness,
+            area,
+            exposedPerimiter,
+            heightAboveGround,
+            rf,
+            wallUValue,
+            ventilation,
+        });
         // Science direct RdSAP value is 0.51
-        expect(result.toFixed(2)).toBe("0.47");
+        expect(result.toFixed(2)).toBe("0.57");
     });
 
     test("Woodfibre insulated floor 100mm Pavaflex", () => {
         // thickness / thermal conductivity
-        const thermalResistance = 0.1 / 0.038;
+        const insulationThermalResistance = 0.1 / 0.038;
 
         // Using the same height above ground as uninsulated floor. Maybe should use 0.25 for the 100mm insulated floor
-        const result = calculate(wallThickness, area, exposedPerimiter, heightAboveGround, thermalResistance);
+        const result = calculate({
+            wallThickness,
+            area,
+            exposedPerimiter,
+            heightAboveGround,
+            rf,
+            wallUValue,
+            ventilation,
+            insulationThermalResistance,
+        });
         // Science direct RdSAP value is 0.22
-        expect(result.toFixed(2)).toBe("0.17");
+        expect(result.toFixed(2)).toBe("0.21");
+    });
+
+    test("Bead insulated floor Warmfill Silver beads", () => {
+        // thickness / thermal conductivity
+        const insulationThermalResistance = 0.25 / 0.033;
+
+        const result = calculate({
+            wallThickness,
+            area,
+            exposedPerimiter,
+            heightAboveGround: 0,
+            rf,
+            wallUValue,
+            ventilation,
+            insulationThermalResistance,
+        });
+        expect(result.toFixed(2)).toBe("0.08");
     });
 });
