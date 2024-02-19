cc46a732e8500eaeddc77b0397ad6c1bcf0e53ca
martin-shaw <martinshaw@outlook.com>
2024-02-19 00:49:27 +0000

    Separate surface area calculation from heatloss calculation

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,6 +1,6 @@
 import { sumAreas } from "./rect";
-import { Room } from "./room";
-import { Surface, calculateUValue } from "./surface";
+import { Room, RoomInRoof } from "./room";
+import { GableWall, Surface, calculateUValue } from "./surface";
 import { defaultRoomTemperature } from "./settings";
 import { Fabric, FabricComponent } from "./fabric";
 import { sum } from "./math";
@@ -10,53 +10,132 @@ interface CalculatedRoom {
     heatLoss: number;
     surfaces: CalculatedSurface[];
 }
+
 interface CalculatedSurface {
     name: string;
-    // uValue: number;
     area: number;
     heatLoss: number;
 }
 
-export const calculateRoom = (room: Room): CalculatedRoom => {
-    const surfaces = room.surfaces.map((surface) => calculateSurface(surface, room.temperature));
+interface CalculatedSurfaceArea {
+    name: string;
+    composition: Fabric | FabricComponent[];
+    area: number;
+}
 
-    return {
-        area: sum(surfaces.map((surface) => surface.area)),
-        heatLoss: sum(surfaces.map((surface) => surface.heatLoss)),
-        surfaces,
-    };
-};
-
-export const calculateSurface = (surface: Surface, roomTemperature: number): CalculatedSurface => {
-    const deltaT =
-        roomTemperature - (surface.boundaryTemperature ? surface.boundaryTemperature : defaultRoomTemperature);
-
-    const surfaces = [surface, ...(surface.elements ? surface.elements : [])];
-
-    const calculatedSurfaces = surfaces.map((surface) => {
-        const uValue = calculateSurfaceUValue(surface.composition);
-        const area = calculateSurfaceArea(surface);
-        // SAP model
-        // https://learn.openenergymonitor.org/sustainable-energy/building-energy-model/fabricheatloss
-        // Heat loss = U-value x Area x Temperature Difference
-        const heatLoss = uValue * area * deltaT;
+export const calculateRoom = (room: Room) => {
+    const calculatedSurfaces = room.surfaces.map((surface) => {
+        const surfaceAreas = calculateSurfaceAreas(surface);
 
         return {
             name: surface.name,
-            uValue,
-            area,
-            heatLoss,
+            area: sum(surfaceAreas.map((surfaceArea) => surfaceArea.area)),
+            heatLoss: sum(
+                surfaceAreas.map((surfaceArea) =>
+                    calculateSurfaceHeatloss(surfaceArea, room.temperature, surface.boundaryTemperature)
+                )
+            ),
         };
     });
 
     return {
-        name: surface.name,
-        area: sum(calculatedSurfaces.map((surface) => surface.area)),
-        heatLoss: sum(calculatedSurfaces.map((surface) => surface.heatLoss)),
+        area: sum(calculatedSurfaces.map((calculatedSurface) => calculatedSurface.area)),
+        heatLoss: sum(calculatedSurfaces.map((calculatedSurface) => calculatedSurface.heatLoss)),
+        surfaces: calculatedSurfaces,
     };
 };
 
+export const calculateRoomInRoof = (room: RoomInRoof): CalculatedRoom => {
+    // const surfaces = room.surfaces.map((surface) => calculateSurface(surface, room.temperature));
+    // const gableWalls = room.gableWalls.map((gableWall) => calculateSurface(gableWall, room.temperature));
+
+    const calculatedSurfaces = room.surfaces.map((surface) => {
+        const surfaceAreas = calculateSurfaceAreas(surface);
+
+        return {
+            name: surface.name,
+            area: sum(surfaceAreas.map((surfaceArea) => surfaceArea.area)),
+            heatLoss: sum(
+                surfaceAreas.map((surfaceArea) =>
+                    calculateSurfaceHeatloss(surfaceArea, room.temperature, surface.boundaryTemperature)
+                )
+            ),
+        };
+    });
+
+    const calculatedGableWalls = room.gableWalls.map((surface) => {
+        const surfaceAreas = calculateSurfaceAreas(surface);
+
+        // Remove roof triangle part from surface area
+
+        return {
+            name: surface.name,
+            area: sum(surfaceAreas.map((surfaceArea) => surfaceArea.area)),
+            heatLoss: sum(
+                surfaceAreas.map((surfaceArea) =>
+                    calculateSurfaceHeatloss(surfaceArea, room.temperature, surface.boundaryTemperature)
+                )
+            ),
+        };
+    });
+
+    return {
+        area: sum(calculatedSurfaces.map((calculatedSurface) => calculatedSurface.area)),
+        heatLoss: sum(calculatedSurfaces.map((calculatedSurface) => calculatedSurface.heatLoss)),
+        surfaces: calculatedSurfaces,
+    };
+};
+
+export const calculateSurfaceAreas = (surface: Surface) => {
+    const flattenedSurfaces = [surface, ...(surface.elements ? surface.elements : [])];
+
+    return flattenedSurfaces.map((surface) => ({
+        name: surface.name,
+        composition: surface.composition,
+        area: calculateSurfaceArea(surface),
+    }));
+};
+
+/*
+export const calculateSurface = (surface: Surface) => {
+    const surfaces = [surface, ...(surface.elements ? surface.elements : [])];
+
+    const calculatedSurfaces = surfaces.map((surface) => calculateSurfaceArea(surface));
+
+    return {
+        name: surface.name,
+        boundaryTemperature: surface.boundaryTemperature,
+        composition: surface.composition,
+        area: sum(calculatedSurfaces),
+    };
+};
+*/
+
+export const calculateSurfaceHeatloss = (
+    surface: CalculatedSurfaceArea,
+    roomTemperature: number,
+    boundaryTemperature?: number
+) => {
+    const deltaT = roomTemperature - (boundaryTemperature ? boundaryTemperature : defaultRoomTemperature);
+
+    const uValue = calculateSurfaceUValue(surface.composition);
+    // SAP model
+    // https://learn.openenergymonitor.org/sustainable-energy/building-energy-model/fabricheatloss
+    // Heat loss = U-value x Area x Temperature Difference
+    return uValue * surface.area * deltaT;
+};
+
 export const calculateSurfaceArea = (surface: Surface) => {
+    let elementsArea = 0;
+
+    if (surface.elements) {
+        elementsArea = sumAreas(surface.elements);
+    }
+
+    return sumAreas([surface]) - elementsArea;
+};
+
+export const calculateGableWallSurfaceArea = (surface: GableWall) => {
     let elementsArea = 0;
 
     if (surface.elements) {
